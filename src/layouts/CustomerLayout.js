1657bfc0e45a71e75d6964808da7a78b4995e821
testdevcits <testdevcits@gmail.com>
2025-10-17 18:12:01 +0530

    Update dashboard, top shippers, review cards & HTML logos

--- src/layouts/CustomerLayout.js
+++ src/layouts/CustomerLayout.js
@@ -24,6 +24,11 @@ const CustomerLayout = () => {
           >
             <CgMenu size={24} />
           </button>
+          <img
+            src={logo}
+            alt="Logo"
+            className="hidden sm:block w-32 h-auto object-contain"
+          />
         </div>
 
         {/* Right: Icons + Profile */}
