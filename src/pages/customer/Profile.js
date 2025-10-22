68e383f8d87947e9fd34925fb350c0a56587dfb9
testdevcits <testdevcits@gmail.com>
2025-10-22 15:17:02 +0530

    fix: sidebar scroll issue; keep sidebar fixed while main content scrolls

--- src/pages/customer/Profile.js
+++ src/pages/customer/Profile.js
@@ -9,7 +9,7 @@ const CustomerProfile = () => {
   const [profilePopup, setProfilePopup] = useState(false);
 
   return (
-    <div className="flex flex-col items-center w-full px-4 sm:px-6 md:px-8 lg:px-16 py-6 space-y-6">
+    <div className="flex flex-col items-center w-full space-y-6">
       {/* Top Profile Image */}
       <div className="relative">
         <img
