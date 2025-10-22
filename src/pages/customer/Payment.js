4570b6666e103448dedd067f6a20e8cd670310c6
testdevcits <testdevcits@gmail.com>
2025-10-22 17:25:34 +0530

    fix: sidebar scroll issue; keep sidebar fixed while main content scrolls

--- src/pages/customer/Payment.js
+++ src/pages/customer/Payment.js
@@ -92,7 +92,7 @@ const Payment = () => {
   return (
     <div className="flex flex-col items-center justify-center font-montserrat">
       <div className="w-full sm:p-8">
-        <h1 className="text-2xl sm:text-3xl font-bold text-center mb-4">
+        <h1 className="text-2xl sm:text-3xl font-bold text-start mb-4">
           Payment
         </h1>
 
