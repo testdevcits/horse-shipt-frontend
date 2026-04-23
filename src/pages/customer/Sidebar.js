aad87099abe48d0146c12bbee5ccdd12b8e853b4
testdevcits <testdevcits@gmail.com>
2026-04-23 17:17:44 +0530

    feat: sync dev with latest updates from main branch

--- src/pages/customer/Sidebar.js
+++ src/pages/customer/Sidebar.js
@@ -35,10 +35,6 @@ const navItems = [
     name: "My Shipments",
     path: "/customer/orders",
     icon: <CustomerOrdersIcon />,
-    // subPaths: [
-    //   { name: "Pending", path: "/customer/orders/pending" },
-    //   { name: "Completed", path: "/customer/orders/completed" },
-    // ],
   },
   {
     name: "New Shipment",
@@ -50,6 +46,14 @@ const navItems = [
     path: "/customer/my-horses",
     icon: <LiaHorseHeadSolid />,
   },
+
+  // NEW TAB
+  {
+    name: "My Quotes",
+    path: "/customer/quotes",
+    icon: <CustomerOrdersIcon />, // change icon if you want
+  },
+
   {
     name: "Chat",
     path: "/customer/chats",
